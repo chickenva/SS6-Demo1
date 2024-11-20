6dab10317c6f5c7b6d22776f9ce38a004856f323
DELL\Vu Anh <vuanh30062004@gmail.com>
2024-11-20 15:55:20 +0700

    first commit

---
+++ src/main/java/com/itproject/vn/BtbsApplication.java
@@ -0,0 +1,13 @@
+package com.itproject.vn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itproject/vn/BtbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itproject.vn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
